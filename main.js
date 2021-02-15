b3330f7d36f9ac4239930263b42635880c4eba78
Thomas <tomaspbury@icloud.com>
2021-02-15 13:58:03 +0000

    Add ability to change read/not read status

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let myLibrary = [
         author: 'George Orwell',
         genre: 'Science Fiction',
         numPages: 435,
-        isRead: false
+        isRead: true
     }
 ];
 
@@ -31,10 +31,13 @@ function Book(title, author, genre, numPages, isRead) {
 }
 
 function addBook() {
+    let formFields = document.querySelectorAll('form input, form select');
     let book = new Book();
     for (let i = 0; i < formFields.length; i++) {
         if (props[i] == 'numPages') {              
             book[props[i]] = `${formFields[i].value}pgs`;
+        } else if (props[i] == 'isRead') {
+            book[props[i]] = formFields[i].checked;
         } else {
             book[props[i]] = formFields[i].value;
         }
@@ -45,22 +48,7 @@ function addBook() {
 
 function updateDisplay() {
     clearDisplay();
-    for (let i = 0; i < myLibrary.length; i++) {         
-        const card = document.createElement('div');
-        card.className = 'card';
-        const cardTop = document.createElement('div');
-        cardTop.className = 'info';
-        createRemoveIcon(cardTop, i);
-        populateCard(cardTop, i);                
-        card.appendChild(cardTop);
-
-        const cardBottom = document.createElement('div');
-        cardBottom.className = 'toggle';
-        cardBottom.textContent = 'Read';
-        card.appendChild(cardBottom);
-
-        section.appendChild(card);
-    }
+    createCards();
 }
 
 
@@ -71,13 +59,42 @@ function clearDisplay() {
     }
 }
 
+function createCards() {
+    for (let i = 0; i < myLibrary.length; i++) {         
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.setAttribute('data-index', i);
+        const cardTop = document.createElement('div');
+        cardTop.className = 'info';
+        createRemoveIcon(cardTop, i);
+        populateCard(cardTop, i);                
+        card.appendChild(cardTop);
+
+        const cardBottom = document.createElement('div');
+        cardBottom.className = 'toggle';
+        if (myLibrary[i].isRead == true) cardBottom.textContent = 'Read';
+        else cardBottom.textContent = 'Not Read';
+        cardBottom.addEventListener('click', (e) => {
+            let index = +e.target.parentNode.getAttribute('data-index');
+            if (cardBottom.textContent == 'Read') {
+                cardBottom.textContent = 'Not Read';
+                myLibrary[index].isRead = false;
+            } else {
+                cardBottom.textContent = 'Read';
+                myLibrary[index].isRead = true;
+            }
+        });
+        card.appendChild(cardBottom);
+
+        section.appendChild(card);
+    }
+}
+
 function createRemoveIcon(cardTop, i) {
     const removeIcon = document.createElement('i');
         removeIcon.className = 'fas fa-trash-alt';
-        removeIcon.setAttribute('data-index', i);
         removeIcon.addEventListener('click', e => {
-            console.log(e.target)
-            let index = +e.target.getAttribute('data-index');
+            let index = +e.target.parentNode.parentNode.getAttribute('data-index');
             myLibrary.splice(index, 1);
             updateDisplay();
         });
@@ -93,19 +110,18 @@ function populateCard(cardTop, i) {
 }
 
 
-/* create global variables */
+/* global variables */
 
 const section = document.querySelector('section');
 const addBtn = document.querySelector('#add-button');
-const formFields = document.querySelectorAll('form input, form select');
 const modalWrapperOuter = document.querySelector('.modal-wrapper-one');
 const modalWrapperInner = document.querySelector('.modal-wrapper-two');
 const formSubmit = document.querySelector('#form-submit-button');
-const props = ['title', 'author', 'genre', 'numPages'];
+const props = Object.keys(myLibrary[0]);
 
 
 
-/* create event handlers */
+/* event handlers */
 
 
 // open modal form
